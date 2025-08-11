f70b9fe4fa4d5cbc9a1ab517adf9bd76a0f672d2
Andresalarcon12 <andresalarcon825@gmail.com>
2025-08-11 17:05:46 -0500

    Agregar proyecto como tutorial_1

---
+++ pages/views.py
@@ -0,0 +1,114 @@
+from django import forms
+from django.shortcuts import render, redirect
+from django.views.generic import TemplateView
+from django.views import View
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
+
+
+class HomePageView(TemplateView):
+    template_name = 'pages/home.html'
+
+
+class AboutPageView(TemplateView):
+    template_name = 'pages/about.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({
+            "title": "About us - Online Store",
+            "subtitle": "About us",
+            "description": "This is an about page ...",
+            "author": "Developed by: Andrés Alarcón",
+        })
+        return context
+
+
+class ContactPageView(TemplateView):
+    template_name = 'pages/contact.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({
+            "title": "Contact - Online Store",
+            "subtitle": "Contact Us",
+            "email": "andres@gmail.com",
+            "address": "calle 3636 norte",
+            "phone": "350643827",
+        })
+        return context
+
+
+
+class Product:
+    products = [
+        {"id": "1", "name": "TV", "description": "Best TV", "price": 150},
+        {"id": "2", "name": "iPhone", "description": "Best iPhone", "price": 999},
+        {"id": "3", "name": "Chromecast", "description": "Best Chromecast", "price": 50},
+        {"id": "4", "name": "Glasses", "description": "Best Glasses", "price": 80},
+    ]
+
+
+class ProductIndexView(View):
+    template_name = 'products/index.html'
+
+    def get(self, request):
+        viewData = {
+            "title": "Products - Online Store",
+            "subtitle": "List of products",
+            "products": Product.products
+        }
+        return render(request, self.template_name, viewData)
+
+
+class ProductShowView(View):
+    template_name = 'products/show.html'
+
+    def get(self, request, id):
+        try:
+            product = Product.products[int(id) - 1]
+        except (IndexError, ValueError):
+            return HttpResponseRedirect(reverse('home'))
+
+        viewData = {
+            "title": f"{product['name']} - Online Store",
+            "subtitle": f"{product['name']} - Product information",
+            "product": product
+        }
+        return render(request, self.template_name, viewData)
+
+
+
+class ProductForm(forms.Form):
+    name = forms.CharField(required=True)
+    price = forms.FloatField(required=True)
+
+class ProductCreateView(View):
+    template_name = 'products/create.html'
+
+    def get(self, request):
+        form = ProductForm()
+        viewData = {
+            "title": "Create product",
+            "form": form
+        }
+        return render(request, self.template_name, viewData)
+
+    def post(self, request):
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            
+            Product.products.append({
+                "id": str(len(Product.products) + 1),
+                "name": form.cleaned_data["name"],
+                "description": "Custom product",
+                "price": form.cleaned_data["price"]
+            })
+            return redirect('index')  
+        else:
+            viewData = {
+                "title": "Create product",
+                "form": form
+            }
+            return render(request, self.template_name, viewData)
